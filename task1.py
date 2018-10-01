f284824d8158571d0cbe77232ac02d42c4b36278
Majid <majid@dal.ca>
2018-10-01 06:52:03 -0700

    Extracting names of interfaces from config

--- task1.py
+++ task1.py
@@ -1,1 +1,21 @@
-#This will have tasl 1 solution
+#My first attempt
+
+
+# Open the file
+
+myfile = open("running-config.cfg")
+
+# Variables for storing stuff
+mylist_intname = []
+
+#Run throught the file and collect the interfacenames
+for line in myfile:
+  # First Remove the whitespaces and extra stuff from the beg and end
+  line = line.strip()
+  # Create a list of strings for each line
+  line = line.split()
+  """ This is the place where we check if list of 0 is "interface" and copy the value of list of 1 """
+  if (line[0] == "interface"):
+    mylist_intname.append(line[1])
+
+print (mylist_intname)
